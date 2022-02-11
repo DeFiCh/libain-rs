47044d54f677b2d910f2f7ae3b64fb85a47980c8
mambisi <lilbizi96@gmail.com>
2022-02-11 14:55:45 +0000

    cargo fmt

--- apps/dex/src/lib.rs
+++ apps/dex/src/lib.rs
@@ -1,5 +1,5 @@
-use std::ops::{Add, Sub};
 use primitive_types::U256;
+use std::ops::{Add, Sub};
 
 use crate::dex::{Error, PoolPair, PoolPrice, SwapResult, TokenAmount};
 
@@ -11,7 +11,7 @@ const PRECISION: u32 = COIN as u32;
 mod dex {
     #[repr(u8)]
     #[derive(Clone, Copy, PartialEq, Eq)]
-    pub enum Error{
+    pub enum Error {
         RuntimeError,
         NotFoundError,
         InvalidInput,
@@ -22,24 +22,14 @@ mod dex {
     impl std::fmt::Debug for Error {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             match self {
-                Error::RuntimeError => {
-                    f.debug_tuple("Error::RuntimeError").finish()
-                }
-                Error::NotFoundError => {
-                    f.debug_tuple("Error::NotFoundError").finish()
-                }
-                Error::InvalidInput => {
-                    f.debug_tuple("Error::InvalidInput").finish()
-                }
-                Error::LackOfLiquidity => {
-                    f.debug_tuple("Error::LackOfLiquidity").finish()
-                }
+                Error::RuntimeError => f.debug_tuple("Error::RuntimeError").finish(),
+                Error::NotFoundError => f.debug_tuple("Error::NotFoundError").finish(),
+                Error::InvalidInput => f.debug_tuple("Error::InvalidInput").finish(),
+                Error::LackOfLiquidity => f.debug_tuple("Error::LackOfLiquidity").finish(),
                 Error::PriceHigherThanIndex => {
                     f.debug_tuple("Error::PriceHigherThanIndex").finish()
                 }
-                Error::PoolReserveOverflow => {
-                    f.debug_tuple("Error::PoolReserveOverflow").finish()
-                }
+                Error::PoolReserveOverflow => f.debug_tuple("Error::PoolReserveOverflow").finish(),
             }
         }
     }
@@ -52,7 +42,11 @@ mod dex {
     }
     impl std::fmt::Debug for TokenAmount {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            f.debug_struct("TokenAmount").field("token-id", &self.token_id).field("amount", &self.amount).finish()}
+            f.debug_struct("TokenAmount")
+                .field("token-id", &self.token_id)
+                .field("amount", &self.amount)
+                .finish()
+        }
     }
     #[repr(C)]
     #[derive(Copy, Clone)]
@@ -62,7 +56,11 @@ mod dex {
     }
     impl std::fmt::Debug for PoolPrice {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            f.debug_struct("PoolPrice").field("integer", &self.integer).field("fraction", &self.fraction).finish()}
+            f.debug_struct("PoolPrice")
+                .field("integer", &self.integer)
+                .field("fraction", &self.fraction)
+                .finish()
+        }
     }
     #[repr(C)]
     #[derive(Copy, Clone)]
@@ -78,7 +76,17 @@ mod dex {
     }
     impl std::fmt::Debug for PoolPair {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            f.debug_struct("PoolPair").field("token-a", &self.token_a).field("token-b", &self.token_b).field("commission", &self.commission).field("reserve-a", &self.reserve_a).field("reserve-b", &self.reserve_b).field("total-liquidity", &self.total_liquidity).field("block-commission-a", &self.block_commission_a).field("block-commission-b", &self.block_commission_b).finish()}
+            f.debug_struct("PoolPair")
+                .field("token-a", &self.token_a)
+                .field("token-b", &self.token_b)
+                .field("commission", &self.commission)
+                .field("reserve-a", &self.reserve_a)
+                .field("reserve-b", &self.reserve_b)
+                .field("total-liquidity", &self.total_liquidity)
+                .field("block-commission-a", &self.block_commission_a)
+                .field("block-commission-b", &self.block_commission_b)
+                .finish()
+        }
     }
     #[repr(C)]
     #[derive(Copy, Clone)]
@@ -88,23 +96,97 @@ mod dex {
     }
     impl std::fmt::Debug for SwapResult {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            f.debug_struct("SwapResult").field("pool-pair", &self.pool_pair).field("slop-swap-result", &self.slop_swap_result).finish()}
+            f.debug_struct("SwapResult")
+                .field("pool-pair", &self.pool_pair)
+                .field("slop-swap-result", &self.slop_swap_result)
+                .finish()
+        }
     }
     #[export_name = "swap"]
-    unsafe extern "C" fn __wit_bindgen_swap(arg0: i32, arg1: i32, arg2: i32, arg3: i64, arg4: i64, arg5: i64, arg6: i64, arg7: i64, arg8: i32, arg9: i64, arg10: i64, arg11: i64, arg12: i32, ) -> i32{
-        let result0 = <super::Dex as Dex>::swap(PoolPair{token_a:arg0 as u32, token_b:arg1 as u32, commission:arg2 as u32, reserve_a:arg3, reserve_b:arg4, total_liquidity:arg5, block_commission_a:arg6, block_commission_b:arg7, }, TokenAmount{token_id:arg8 as u32, amount:arg9, }, PoolPrice{integer:arg10, fraction:arg11, }, match arg12 {
-            0 => false,
-            1 => true,
-            _ => panic!("invalid enum discriminant"),
-        });
-        let (result3_0,result3_1,result3_2,result3_3,result3_4,result3_5,result3_6,result3_7,result3_8,result3_9,) = match result0{
-            Ok(e) => { {
-                let SwapResult{ pool_pair:pool_pair1, slop_swap_result:slop_swap_result1, } = e;
-                let PoolPair{ token_a:token_a2, token_b:token_b2, commission:commission2, reserve_a:reserve_a2, reserve_b:reserve_b2, total_liquidity:total_liquidity2, block_commission_a:block_commission_a2, block_commission_b:block_commission_b2, } = pool_pair1;
+    unsafe extern "C" fn __wit_bindgen_swap(
+        arg0: i32,
+        arg1: i32,
+        arg2: i32,
+        arg3: i64,
+        arg4: i64,
+        arg5: i64,
+        arg6: i64,
+        arg7: i64,
+        arg8: i32,
+        arg9: i64,
+        arg10: i64,
+        arg11: i64,
+        arg12: i32,
+    ) -> i32 {
+        let result0 = <super::Dex as Dex>::swap(
+            PoolPair {
+                token_a: arg0 as u32,
+                token_b: arg1 as u32,
+                commission: arg2 as u32,
+                reserve_a: arg3,
+                reserve_b: arg4,
+                total_liquidity: arg5,
+                block_commission_a: arg6,
+                block_commission_b: arg7,
+            },
+            TokenAmount {
+                token_id: arg8 as u32,
+                amount: arg9,
+            },
+            PoolPrice {
+                integer: arg10,
+                fraction: arg11,
+            },
+            match arg12 {
+                0 => false,
+                1 => true,
+                _ => panic!("invalid enum discriminant"),
+            },
+        );
+        let (
+            result3_0,
+            result3_1,
+            result3_2,
+            result3_3,
+            result3_4,
+            result3_5,
+            result3_6,
+            result3_7,
+            result3_8,
+            result3_9,
+        ) = match result0 {
+            Ok(e) => {
+                let SwapResult {
+                    pool_pair: pool_pair1,
+                    slop_swap_result: slop_swap_result1,
+                } = e;
+                let PoolPair {
+                    token_a: token_a2,
+                    token_b: token_b2,
+                    commission: commission2,
+                    reserve_a: reserve_a2,
+                    reserve_b: reserve_b2,
+                    total_liquidity: total_liquidity2,
+                    block_commission_a: block_commission_a2,
+                    block_commission_b: block_commission_b2,
+                } = pool_pair1;
 
-                (0i32, wit_bindgen_rust::rt::as_i32(token_a2), wit_bindgen_rust::rt::as_i32(token_b2), wit_bindgen_rust::rt::as_i32(commission2), wit_bindgen_rust::rt::as_i64(reserve_a2), wit_bindgen_rust::rt::as_i64(reserve_b2), wit_bindgen_rust::rt::as_i64(total_liquidity2), wit_bindgen_rust::rt::as_i64(block_commission_a2), wit_bindgen_rust::rt::as_i64(block_commission_b2), wit_bindgen_rust::rt::as_i64(slop_swap_result1))
-            }}
-            Err(e) => { (1i32, e as i32, 0i32, 0i32, 0i64, 0i64, 0i64, 0i64, 0i64, 0i64)}
+                (
+                    0i32,
+                    wit_bindgen_rust::rt::as_i32(token_a2),
+                    wit_bindgen_rust::rt::as_i32(token_b2),
+                    wit_bindgen_rust::rt::as_i32(commission2),
+                    wit_bindgen_rust::rt::as_i64(reserve_a2),
+                    wit_bindgen_rust::rt::as_i64(reserve_b2),
+                    wit_bindgen_rust::rt::as_i64(total_liquidity2),
+                    wit_bindgen_rust::rt::as_i64(block_commission_a2),
+                    wit_bindgen_rust::rt::as_i64(block_commission_b2),
+                    wit_bindgen_rust::rt::as_i64(slop_swap_result1),
+                )
+            }
+            Err(e) => (
+                1i32, e as i32, 0i32, 0i32, 0i64, 0i64, 0i64, 0i64, 0i64, 0i64,
+            ),
         };
         let ptr4 = RET_AREA.as_mut_ptr() as i32;
         *((ptr4 + 72) as *mut i64) = result3_9;
@@ -120,12 +202,15 @@ mod dex {
         ptr4
     }
     pub trait Dex {
-        fn swap(poolpair: PoolPair,token_in: TokenAmount,max_price: PoolPrice,post_bayfront_gardens: bool,) -> Result<SwapResult,Error>;
+        fn swap(
+            poolpair: PoolPair,
+            token_in: TokenAmount,
+            max_price: PoolPrice,
+            post_bayfront_gardens: bool,
+        ) -> Result<SwapResult, Error>;
     }
     static mut RET_AREA: [i64; 10] = [0; 10];
 }
-
-
 
 impl Add for TokenAmount {
     type Output = TokenAmount;
@@ -133,7 +218,7 @@ impl Add for TokenAmount {
     fn add(self, rhs: Self) -> Self::Output {
         TokenAmount {
             token_id: self.token_id,
-            amount: self.amount.saturating_add(rhs.amount)
+            amount: self.amount.saturating_add(rhs.amount),
         }
     }
 }
@@ -144,7 +229,7 @@ impl Sub for TokenAmount {
     fn sub(self, rhs: Self) -> Self::Output {
         TokenAmount {
             token_id: self.token_id,
-            amount: self.amount.saturating_sub(rhs.amount)
+            amount: self.amount.saturating_sub(rhs.amount),
         }
     }
 }
@@ -155,14 +240,21 @@ impl PartialEq for TokenAmount {
     }
 }
 
-
 struct Dex {}
 
 impl dex::Dex for Dex {
-    fn swap(poolpair: PoolPair, token_in: TokenAmount, max_price: PoolPrice, post_bayfront_gardens: bool) -> Result<SwapResult, Error> {
+    fn swap(
+        poolpair: PoolPair,
+        token_in: TokenAmount,
+        max_price: PoolPrice,
+        post_bayfront_gardens: bool,
+    ) -> Result<SwapResult, Error> {
         let mut poolpair = poolpair;
         if token_in.token_id != poolpair.token_a && token_in.token_id != poolpair.token_b {
-            panic!("Error, input token ID ({}) doesn't match pool tokens ({})", poolpair.token_a, poolpair.token_b);
+            panic!(
+                "Error, input token ID ({}) doesn't match pool tokens ({})",
+                poolpair.token_a, poolpair.token_b
+            );
         }
         if token_in.amount <= 0 {
             return Err(Error::InvalidInput);
@@ -187,13 +279,30 @@ impl dex::Dex for Dex {
             return Err(Error::PriceHigherThanIndex);
         }
 
-
-        let check_res = if forward { poolpair.reserve_a.checked_add(token_in.amount) } else { poolpair.reserve_b.checked_add(token_in.amount) };
+        let check_res = if forward {
+            poolpair.reserve_a.checked_add(token_in.amount)
+        } else {
+            poolpair.reserve_b.checked_add(token_in.amount)
+        };
         if check_res.is_none() {
             return Err(Error::PoolReserveOverflow);
         }
 
-        let result = if forward { Dex::slop_swap(token_in.amount, &mut poolpair.reserve_a, &mut poolpair.reserve_b, post_bayfront_gardens) } else { Dex::slop_swap(token_in.amount, &mut poolpair.reserve_b, &mut poolpair.reserve_a, post_bayfront_gardens) };
+        let result = if forward {
+            Dex::slop_swap(
+                token_in.amount,
+                &mut poolpair.reserve_a,
+                &mut poolpair.reserve_b,
+                post_bayfront_gardens,
+            )
+        } else {
+            Dex::slop_swap(
+                token_in.amount,
+                &mut poolpair.reserve_b,
+                &mut poolpair.reserve_a,
+                post_bayfront_gardens,
+            )
+        };
 
         Ok(SwapResult {
             pool_pair: poolpair,
@@ -202,9 +311,13 @@ impl dex::Dex for Dex {
     }
 }
 
-
 impl Dex {
-    pub fn slop_swap(unswapped: i64, pool_from: &mut i64, pool_to: &mut i64, post_bayfront_gardens: bool) -> i64 {
+    pub fn slop_swap(
+        unswapped: i64,
+        pool_from: &mut i64,
+        pool_to: &mut i64,
+        post_bayfront_gardens: bool,
+    ) -> i64 {
         let mut unswapped = unswapped;
 
         assert!(unswapped >= 0);
@@ -216,7 +329,11 @@ impl Dex {
         let mut swapped: U256 = 0.into();
 
         if post_bayfront_gardens {
-            let chunk = if *pool_from / SLOPE_SWAP_RATE < unswapped { *pool_from / SLOPE_SWAP_RATE } else { unswapped };
+            let chunk = if *pool_from / SLOPE_SWAP_RATE < unswapped {
+                *pool_from / SLOPE_SWAP_RATE
+            } else {
+                unswapped
+            };
             while unswapped > 0 {
                 let step_from = (chunk).min(unswapped);
                 let step_from_256 = U256::from(step_from);
--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,39 +1,50 @@
 mod dex_bindings;
 
+use crate::dex_bindings::{Dex, DexData, PoolPair, PoolPrice, SwapResult, TokenAmount};
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 use std::path::Path;
 use wasmtime_wasi::{WasiCtx, WasiCtxBuilder};
+use wit_bindgen_wasmtime::anyhow::{anyhow, bail, Result};
 use wit_bindgen_wasmtime::wasmtime::*;
-use wit_bindgen_wasmtime::anyhow::{Result, bail, anyhow};
-use crate::dex_bindings::{Dex, DexData, PoolPair, PoolPrice, SwapResult, TokenAmount};
 
 #[no_mangle]
-pub extern "C" fn ainrt_execute_dex_swap( dex_module_file_path : *const c_char, poolpair: *mut PoolPair, token_in: &TokenAmount, max_price: &PoolPrice, post_bayfront_gardens: bool, result: *mut SwapResult) -> i32 {
-
+pub extern "C" fn ainrt_execute_dex_swap(
+    dex_module_file_path: *const c_char,
+    poolpair: *mut PoolPair,
+    token_in: &TokenAmount,
+    max_price: &PoolPrice,
+    post_bayfront_gardens: bool,
+    result: *mut SwapResult,
+) -> i32 {
     let pp = unsafe { *poolpair.clone() };
     let tk_in = unsafe { token_in.clone() };
     let mp = unsafe { max_price.clone() };
-    let dex_module_file_path = unsafe { CStr::from_ptr(dex_module_file_path) }.to_str().unwrap();
+    let dex_module_file_path = unsafe { CStr::from_ptr(dex_module_file_path) }
+        .to_str()
+        .unwrap();
 
     match dex_swap(dex_module_file_path, pp, tk_in, mp, post_bayfront_gardens) {
         Ok(res) => {
-            unsafe { *poolpair = res.pool_pair}
+            unsafe { *poolpair = res.pool_pair }
             unsafe { *result = res }
             1
         }
-        Err(_) => {
-            0
-        }
+        Err(_) => 0,
     }
 }
 
 struct Context {
-    pub wasi : WasiCtx
+    pub wasi: WasiCtx,
 }
 
-
-fn dex_swap<P: AsRef<Path>>(path: P, poolpair: PoolPair, token_in: TokenAmount, max_price: PoolPrice, post_bayfront_gardens: bool) -> Result<SwapResult> {
+fn dex_swap<P: AsRef<Path>>(
+    path: P,
+    poolpair: PoolPair,
+    token_in: TokenAmount,
+    max_price: PoolPrice,
+    post_bayfront_gardens: bool,
+) -> Result<SwapResult> {
     let engine = Engine::default();
     let mut linker = Linker::new(&engine);
     wasmtime_wasi::add_to_linker(&mut linker, |s| s)?;
@@ -45,6 +56,12 @@ fn dex_swap<P: AsRef<Path>>(path: P, poolpair: PoolPair, token_in: TokenAmount, 
 
     let mut store = Store::new(&engine, wasi);
     let (dex, _instance) = Dex::instantiate(&mut store, &dex_module, &mut linker)?;
-    let result = dex.swap(&mut store, poolpair, token_in, max_price, post_bayfront_gardens)?;
+    let result = dex.swap(
+        &mut store,
+        poolpair,
+        token_in,
+        max_price,
+        post_bayfront_gardens,
+    )?;
     result.map_err(|e| anyhow!(format!("{:?}", e)))
 }
