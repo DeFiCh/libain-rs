d9bbdb053e5383e76fa4f1fa1bb41759ed3fa89b
jouzo <jdesclercs@gmail.com>
2022-04-06 17:48:24 +0100

    Fix clippy warnings

--- wasm-runtime/src/lib.rs
+++ wasm-runtime/src/lib.rs
@@ -9,7 +9,7 @@ use std::path::Path;
 use std::sync::Arc;
 use wasmtime_wasi::{WasiCtx, WasiCtxBuilder};
 use wit_bindgen_wasmtime::anyhow::{anyhow, Result};
-use wit_bindgen_wasmtime::wasmtime::*;
+use wit_bindgen_wasmtime::wasmtime::{Engine, Instance, Linker, Module, Store};
 
 const DEX_MODULE_ID: &str = "dex";
 
@@ -18,11 +18,12 @@ lazy_static! {
     static ref STOREMAP: Arc<DashMap<&'static str, Store<WasiCtx>>> = Arc::new(DashMap::new());
 }
 
+/// # Safety
+///
+/// Should validate file path
 #[no_mangle]
-pub extern "C" fn ainrt_register_dex_module(dex_module_file_path: *const c_char) -> i32 {
-    let dex_module_file_path = unsafe { CStr::from_ptr(dex_module_file_path) }
-        .to_str()
-        .unwrap();
+pub unsafe extern "C" fn ainrt_register_dex_module(dex_module_file_path: *const c_char) -> i32 {
+    let dex_module_file_path = CStr::from_ptr(dex_module_file_path).to_str().unwrap();
     match register_dex_module(dex_module_file_path) {
         Ok(_) => 1,
         Err(_) => 0,
@@ -46,17 +47,20 @@ fn register_dex_module<P: AsRef<Path>>(path: P) -> Result<()> {
     Ok(())
 }
 
+/// # Safety
+///
+/// TODO
 #[no_mangle]
-pub extern "C" fn ainrt_call_dex_swap(
+pub unsafe extern "C" fn ainrt_call_dex_swap(
     poolpair: *mut PoolPair,
     token_in: &TokenAmount,
     max_price: &PoolPrice,
     post_bayfront_gardens: bool,
 ) -> i64 {
-    let pp = unsafe { *poolpair };
+    let pp = *poolpair;
     match dex_swap(pp, *token_in, *max_price, post_bayfront_gardens) {
         Ok(res) => {
-            unsafe { *poolpair = res.pool_pair }
+            *poolpair = res.pool_pair;
             res.slop_swap_result
         }
         Err(_) => 0,
@@ -94,7 +98,7 @@ mod tests {
     use crate::{dex_swap, register_dex_module, PoolPair, PoolPrice, TokenAmount};
     use std::path::PathBuf;
     use std::time::Instant;
-    const COIN: i64 = 100000000;
+    const COIN: i64 = 100_000_000;
     #[test]
     fn text_swap() {
         let gold = 1;
