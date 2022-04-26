f03fe239b9a807969899457c10a25e517383e2e4
mambisi <lilbizi96@gmail.com>
2022-04-26 07:41:25 +0000

    customview added

--- defichain-ffi/build.rs
+++ defichain-ffi/build.rs
@@ -21,22 +21,8 @@ fn build_leveldb() {
     println!("cargo:rustc-link-lib=static=crc32c_sse42");
 
     println!("cargo:rustc-link-search={}", project_dir.join("depend/ain/src").display());
-    // println!("cargo:rustc-link-lib=static=defi_server");
-    // println!("cargo:rustc-link-lib=static=defi_common");
-    // println!("cargo:rustc-link-lib=static=defi_spv");
-    //println!("cargo:rustc-link-lib=static=defi_wallet");
-    // println!("cargo:rustc-link-lib=static=defi_zmq");
-    // println!("cargo:rustc-link-lib=static=defi_util");
-    // println!("cargo:rustc-link-lib=static=defi_consensus");
-    //
-    //
-    // println!("cargo:rustc-link-search={}",project_dir.join("depend/ain/src/crypto").display());
-    // println!("cargo:rustc-link-lib=static=defi_crypto_avx2");
-    // println!("cargo:rustc-link-lib=static=defi_crypto_base");
-    // println!("cargo:rustc-link-lib=static=defi_crypto_shani");
-    // println!("cargo:rustc-link-lib=static=defi_crypto_sse41");
 
-    /// Dependencies
+    // Dependencies, TODO : make it universal for other other os builds
     Command::new("tar")
         .args([
             "-xf",
@@ -65,13 +51,7 @@ fn build_leveldb() {
     println!("cargo:rustc-link-lib=static=boost_atomic-mt-x64");
     println!("cargo:rustc-link-lib=static=boost_filesystem-mt-x64");
     println!("cargo:rustc-link-lib=static=event");
-    //println!("cargo:rustc-link-lib=static=event_core");
-    //println!("cargo:rustc-link-lib=static=event_extra");
-    //println!("cargo:rustc-link-lib=static=event_pthreads");
-    //println!("cargo:rustc-link-lib=static=db");
-    //println!("cargo:rustc-link-lib=static=db-4.8");
     println!("cargo:rustc-link-lib=static=db_cxx");
-   // println!("cargo:rustc-link-lib=static=db_cxx-4.8");
 }
 
 
@@ -96,54 +76,62 @@ fn main() {
 
     // Master node files
     base_config.file("depend/ain/src/masternodes/accounts.cpp");
-    // base_config.file("depend/ain/src/masternodes/accountshistory.cpp");
-    // base_config.file("depend/ain/src/masternodes/anchors.cpp");
-    // base_config.file("depend/ain/src/masternodes/auctionhistory.cpp");
-    // base_config.file("depend/ain/src/masternodes/consensus/accounts.cpp");
-    // base_config.file("depend/ain/src/masternodes/consensus/governance.cpp");
-    // base_config.file("depend/ain/src/masternodes/consensus/icxorders.cpp");
-    // base_config.file("depend/ain/src/masternodes/consensus/loans.cpp");
-    // base_config.file("depend/ain/src/masternodes/consensus/masternodes.cpp");
-    // base_config.file("depend/ain/src/masternodes/consensus/oracles.cpp");
-    // base_config.file("depend/ain/src/masternodes/consensus/poolpairs.cpp");
-    // base_config.file("depend/ain/src/masternodes/consensus/smartcontracts.cpp");
-    // base_config.file("depend/ain/src/masternodes/consensus/tokens.cpp");
-    // base_config.file("depend/ain/src/masternodes/consensus/txvisitor.cpp");
-    // base_config.file("depend/ain/src/masternodes/consensus/vaults.cpp");
-    // base_config.file("depend/ain/src/masternodes/govvariables/attributes.cpp");
-    // base_config.file("depend/ain/src/masternodes/govvariables/icx_takerfee_per_btc.cpp");
-    // base_config.file("depend/ain/src/masternodes/govvariables/loan_daily_reward.cpp");
-    // base_config.file("depend/ain/src/masternodes/govvariables/loan_liquidation_penalty.cpp");
-    // base_config.file("depend/ain/src/masternodes/govvariables/loan_splits.cpp");
-    // base_config.file("depend/ain/src/masternodes/govvariables/lp_daily_dfi_reward.cpp");
-    // base_config.file("depend/ain/src/masternodes/govvariables/lp_splits.cpp");
-    // base_config.file("depend/ain/src/masternodes/govvariables/oracle_block_interval.cpp");
-    // base_config.file("depend/ain/src/masternodes/govvariables/oracle_deviation.cpp");
-    // base_config.file("depend/ain/src/masternodes/gv.cpp");
-    // base_config.file("depend/ain/src/masternodes/icxorder.cpp");
-    // base_config.file("depend/ain/src/masternodes/incentivefunding.cpp");
-    // base_config.file("depend/ain/src/masternodes/loan.cpp");
-    // base_config.file("depend/ain/src/masternodes/masternodes.cpp");
-    // base_config.file("depend/ain/src/masternodes/oracles.cpp");
-    // base_config.file("depend/ain/src/masternodes/poolpairs.cpp");
-    // base_config.file("depend/ain/src/masternodes/skipped_txs.cpp");
-    // base_config.file("depend/ain/src/masternodes/tokens.cpp");
-    // base_config.file("depend/ain/src/masternodes/undos.cpp");
-    // base_config.file("depend/ain/src/masternodes/vault.cpp");
-    // base_config.file("depend/ain/src/masternodes/vaulthistory.cpp");
+    base_config.file("depend/ain/src/masternodes/accountshistory.cpp");
+    base_config.file("depend/ain/src/masternodes/anchors.cpp");
+    base_config.file("depend/ain/src/masternodes/auctionhistory.cpp");
+    base_config.file("depend/ain/src/masternodes/consensus/accounts.cpp");
+    base_config.file("depend/ain/src/masternodes/consensus/governance.cpp");
+    base_config.file("depend/ain/src/masternodes/consensus/icxorders.cpp");
+    base_config.file("depend/ain/src/masternodes/consensus/loans.cpp");
+    base_config.file("depend/ain/src/masternodes/consensus/masternodes.cpp");
+    base_config.file("depend/ain/src/masternodes/consensus/oracles.cpp");
+    base_config.file("depend/ain/src/masternodes/consensus/poolpairs.cpp");
+    base_config.file("depend/ain/src/masternodes/consensus/smartcontracts.cpp");
+    base_config.file("depend/ain/src/masternodes/consensus/tokens.cpp");
+    base_config.file("depend/ain/src/masternodes/consensus/txvisitor.cpp");
+    base_config.file("depend/ain/src/masternodes/consensus/vaults.cpp");
+    base_config.file("depend/ain/src/masternodes/govvariables/attributes.cpp");
+    base_config.file("depend/ain/src/masternodes/govvariables/icx_takerfee_per_btc.cpp");
+    base_config.file("depend/ain/src/masternodes/govvariables/loan_daily_reward.cpp");
+    base_config.file("depend/ain/src/masternodes/govvariables/loan_liquidation_penalty.cpp");
+    base_config.file("depend/ain/src/masternodes/govvariables/loan_splits.cpp");
+    base_config.file("depend/ain/src/masternodes/govvariables/lp_daily_dfi_reward.cpp");
+    base_config.file("depend/ain/src/masternodes/govvariables/lp_splits.cpp");
+    base_config.file("depend/ain/src/masternodes/govvariables/oracle_block_interval.cpp");
+    base_config.file("depend/ain/src/masternodes/govvariables/oracle_deviation.cpp");
+    base_config.file("depend/ain/src/masternodes/gv.cpp");
+    base_config.file("depend/ain/src/masternodes/icxorder.cpp");
+    base_config.file("depend/ain/src/masternodes/incentivefunding.cpp");
+    base_config.file("depend/ain/src/masternodes/loan.cpp");
+    base_config.file("depend/ain/src/masternodes/masternodes.cpp");
+    base_config.file("depend/ain/src/masternodes/oracles.cpp");
+    base_config.file("depend/ain/src/masternodes/poolpairs.cpp");
+    base_config.file("depend/ain/src/masternodes/skipped_txs.cpp");
+    base_config.file("depend/ain/src/masternodes/tokens.cpp");
+    base_config.file("depend/ain/src/masternodes/undos.cpp");
+    base_config.file("depend/ain/src/masternodes/vault.cpp");
+    base_config.file("depend/ain/src/masternodes/mn_checks.cpp");
+    base_config.file("depend/ain/src/masternodes/vaulthistory.cpp");
     // Wrapper
-    // base_config.file("depend/wrapper/wrapper.cpp");
-    //base_config.file("depend/ain/src/masternodes/rpc_accounts.cpp");
+    base_config.file("depend/wrapper/wrapper.cpp");
+    //Script
+    base_config.file("depend/ain/src/script/script.cpp");
+    base_config.file("depend/ain/src/script/standard.cpp");
+    base_config.file("depend/ain/src/script/interpreter.cpp");
+    base_config.file("depend/ain/src/script/script_error.cpp");
 
     base_config.file("depend/ain/src/flushablestorage.cpp");
     base_config.file("depend/ain/src/dbwrapper.cpp");
-    base_config.file("depend/ain/src/script/script.cpp");
     base_config.file("depend/ain/src/logging.cpp");
     base_config.file("depend/ain/src/fs.cpp");
+    base_config.file("depend/ain/src/key_io.cpp");
+    base_config.file("depend/ain/src/base58.cpp");
+    base_config.file("depend/ain/src/bech32.cpp");
+    base_config.file("depend/ain/src/arith_uint256.cpp");
     base_config.file("depend/ain/src/random.cpp");
-    // base_config.file("depend/ain/src/core_read.cpp");
-    // base_config.file("depend/ain/src/core_write.cpp");
-    // base_config.file("depend/ain/src/chainparams.cpp");
+    base_config.file("depend/ain/src/core_read.cpp");
+    base_config.file("depend/ain/src/core_write.cpp");
+    base_config.file("depend/ain/src/chainparams.cpp");
     base_config.file("depend/ain/src/chainparamsbase.cpp");
 
     base_config.file("depend/ain/src/support/cleanse.cpp");
@@ -156,15 +144,16 @@ fn main() {
     base_config.file("depend/ain/src/util/system.cpp");
     base_config.file("depend/ain/src/util/threadnames.cpp");
 
+    //rpc
+    base_config.file("depend/ain/src/rpc/request.cpp");
+
     base_config.file("depend/cxx/src/cxx.cc");
     base_config.file("depend/ain/src/crypto/sha256.cpp");
     base_config.file("depend/ain/src/crypto/sha512.cpp");
+    base_config.file("depend/ain/src/crypto/ripemd160.cpp");
     base_config.file("depend/ain/src/uint256.cpp");
     base_config.file("depend/ain/src/hash.cpp");
     base_config.file("depend/ain/src/crypto/sha1.cpp");
 
     base_config.build("src/lib.rs");
-
-    // println!("cargo:rustc-link-lib=boost_system");
-    // println!("cargo:rustc-link-lib=boost_filesystem");
 }
--- defichain-ffi/depend/wrapper/wrapper.cpp
+++ defichain-ffi/depend/wrapper/wrapper.cpp
@@ -6,6 +6,7 @@
 #include <amount.h>
 #include <string>
 #include <masternodes/masternodes.h>
+#include <util/strencodings.h>
 
 std::string tokenAmountString(CTokenAmount const& amount) {
     const auto token = pcustomcsview->GetToken(amount.nTokenId);
@@ -28,4 +29,72 @@ std::string ScriptToString(CScript const& script) {
         return script.GetHex();
     }
     return EncodeDestination(dest);
+}
+
+CAmount AmountFromValue(const UniValue& value)
+{
+    if (!value.isNum() && !value.isStr())
+        throw std::runtime_error("Amount is not a number or string");
+    CAmount amount;
+    if (!ParseFixedPoint(value.getValStr(), 8, &amount))
+        throw std::runtime_error("Invalid amount");
+    if (!MoneyRange(amount))
+        throw std::runtime_error("Amount out of range");
+    return amount;
+}
+
+// Found in src/masternodes/rpc_oracles.cpp
+
+bool diffInHour(int64_t time1, int64_t time2) {
+    constexpr const int64_t SECONDS_PER_HOUR = 3600u;
+    return std::abs(time1 - time2) < SECONDS_PER_HOUR;
+}
+
+// Found in src/masternodes/rpc_oracles.cpp
+ResVal<CAmount> GetAggregatePrice(CCustomCSView& view, const std::string& token, const std::string& currency, uint64_t lastBlockTime) {
+    // DUSD-USD always returns 1.00000000
+    if (token == "DUSD" && currency == "USD") {
+        return ResVal<CAmount>(COIN, Res::Ok());
+    }
+    arith_uint256 weightedSum = 0;
+    uint64_t numLiveOracles = 0, sumWeights = 0;
+    view.ForEachOracle([&](const COracleId&, COracle oracle) {
+        if (!oracle.SupportsPair(token, currency)) {
+            return true;
+        }
+        for (const auto& tokenPrice : oracle.tokenPrices) {
+            if (token != tokenPrice.first) {
+                continue;
+            }
+            for (const auto& price : tokenPrice.second) {
+                if (currency != price.first) {
+                    continue;
+                }
+                const auto& pricePair = price.second;
+                auto amount = pricePair.first;
+                auto timestamp = pricePair.second;
+                if (!diffInHour(timestamp, lastBlockTime)) {
+                    continue;
+                }
+                ++numLiveOracles;
+                sumWeights += oracle.weightage;
+                weightedSum += arith_uint256(amount) * oracle.weightage;
+            }
+        }
+        return true;
+    });
+
+    static const uint64_t minimumLiveOracles = Params().NetworkIDString() == CBaseChainParams::REGTEST ? 1 : 2;
+
+    if (numLiveOracles < minimumLiveOracles) {
+        return Res::Err("no live oracles for specified request");
+    }
+
+    if (sumWeights == 0) {
+        return Res::Err("all live oracles which meet specified request, have zero weight");
+    }
+
+    ResVal<CAmount> res((weightedSum / sumWeights).GetLow64(), Res::Ok());
+    LogPrint(BCLog::LOAN, "%s(): %s/%s=%lld\n", __func__, token, currency, *res.val);
+    return res;
 }
