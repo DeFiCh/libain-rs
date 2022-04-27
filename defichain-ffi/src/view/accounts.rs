15d18d694e57df09479ef425341eff66a2a3a798
mambisi <lilbizi96@gmail.com>
2022-04-27 13:52:41 +0000

    use pointer to `CStorageView`

--- defichain-ffi/src/view/accounts.rs
+++ defichain-ffi/src/view/accounts.rs
@@ -1,38 +1,34 @@
 use cpp::{cpp, cpp_class};
 use crate::types::*;
+use crate::view::CustomStorageView;
 
 cpp! {{
     #include <masternodes/accounts.h>
     #include <flushablestorage.h>
-    #include <iostream>
-    #include <stdio.h>
 }}
 
-cpp_class!(pub unsafe struct CustomDB as "std::shared_ptr<CStorageKV>");
-
-pub struct Accounts {
-    db: CustomDB,
+pub trait AccountsView {
+    fn set_balance(&self, owner: Script, amount: TokenAmount);
+    fn get_balance(&self, owner: Script, token_id: DctId) -> TokenAmount;
 }
 
 
-impl Accounts {
-    pub fn set_balance(&self, owner: Script, amount: TokenAmount) {
+impl AccountsView for CustomStorageView {
+    fn set_balance(&self, owner: Script, amount: TokenAmount) {
         unsafe {
-            let db = self.db.clone();
-            cpp!([ db as "std::shared_ptr<CStorageKV>", owner as "CScript", amount as "CTokenAmount"] {
-                CStorageView sview = CStorageView(db);
-                sview.WriteBy<CAccountsView::ByBalanceKey>(BalanceKey{owner, amount.nTokenId}, amount.nValue);
+            let db = &self.inner;
+            cpp!([ db as "std::unique_ptr<CStorageView>*", owner as "CScript", amount as "CTokenAmount"] {
+                (*db)->WriteBy<CAccountsView::ByBalanceKey>(BalanceKey{owner, amount.nTokenId}, amount.nValue);
             })
         };
     }
 
-    pub fn get_balance(&self, owner: Script, token_id: DctId) -> TokenAmount {
+    fn get_balance(&self, owner: Script, token_id: DctId) -> TokenAmount {
         unsafe {
-            let db = self.db.clone();
-            cpp!([ db as "std::shared_ptr<CStorageKV>", owner as "CScript", token_id as "DCT_ID"] -> TokenAmount as "CTokenAmount" {
+            let db = &self.inner;
+            cpp!([ db as "std::unique_ptr<CStorageView>*", owner as "CScript", token_id as "DCT_ID"] -> TokenAmount as "CTokenAmount" {
                 CAmount val;
-                CStorageView sview = CStorageView(db);
-                bool ok = sview.ReadBy<CAccountsView::ByBalanceKey>(BalanceKey{owner, token_id}, val);
+                bool ok = (*db)->ReadBy<CAccountsView::ByBalanceKey>(BalanceKey{owner, token_id}, val);
                 if (ok) {
                     return CTokenAmount{token_id, val};
                 }
@@ -42,26 +38,17 @@ impl Accounts {
     }
 }
 
-impl CustomDB {
-    pub fn flush(self)  {
-        unsafe {
-            cpp!([self as "std::shared_ptr<CStorageKV>"]{
-                CStorageView sview = CStorageView(self);
-                sview.Flush();
-            })
-        }
-    }
-}
 
 #[cfg(test)]
 mod test {
     use tempdir::TempDir;
+    use crate::view::RawStorageKVPtr;
     use super::*;
 
-    fn init_test_storage(dir: &str) -> CustomDB {
+    fn init_test_storage(dir: &str) -> RawStorageKVPtr {
         unsafe {
             let path = dir.to_string();
-            cpp!([path as "rust::String"] -> CustomDB as "std::shared_ptr<CStorageKV>"{
+            cpp!([path as "rust::String"] -> RawStorageKVPtr as "std::shared_ptr<CStorageKV>"{
                 std::string std_path = std::string(path);
                 auto pcustomcsDB = std::make_shared<CStorageKV>(CStorageLevelDB(std_path, 3000000, false, false));
                 return pcustomcsDB;
@@ -75,12 +62,10 @@ mod test {
     fn test_account_storage() {
         let tmp_dir = TempDir::new("__test__test_account_storage__").expect("unable to create test directory");
         let db = init_test_storage(tmp_dir.path().to_str().unwrap());
-        let accounts = Accounts {
-            db : db.clone()
-        };
+        let mnview = CustomStorageView::new(db);
         let script = Script::from_vec(vec![1, 2, 3, 4]);
-        accounts.set_balance(script.clone(), TokenAmount::new(DctId::new(0), 10000));
-        db.flush();
-        assert_eq!(accounts.get_balance(script, DctId::new(0)), TokenAmount::new(DctId::new(0), 10000))
+        mnview.set_balance(script.clone(), TokenAmount::new(DctId::new(0), 10000));
+        mnview.flush();
+        assert_eq!(mnview.get_balance(script, DctId::new(0)), TokenAmount::new(DctId::new(0), 10000))
     }
 }
--- defichain-ffi/src/view/mod.rs
+++ defichain-ffi/src/view/mod.rs
@@ -1,1 +1,38 @@
+use cpp::*;
+
 mod accounts;
+
+cpp! {{
+    #include <masternodes/accounts.h>
+    #include <flushablestorage.h>
+}}
+
+
+cpp_class!(pub unsafe struct RawStorageKVPtr as "std::shared_ptr<CStorageKV>");
+cpp_class!(pub unsafe struct StorageViewPtr as "std::unique_ptr<CStorageView>");
+
+pub struct CustomStorageView {
+    inner : StorageViewPtr
+}
+
+/// State
+impl CustomStorageView {
+    pub fn new(kv: RawStorageKVPtr) -> Self {
+        let db = cpp!(unsafe [kv as "std::shared_ptr<CStorageKV>"] -> StorageViewPtr as "std::unique_ptr<CStorageView>"{
+            std::unique_ptr<CStorageView> storage_view = std::make_unique<CStorageView>(kv);
+            return storage_view;
+        });
+
+        Self {
+            inner: db
+        }
+    }
+
+
+    pub fn flush(&self) {
+        let db = &self.inner;
+        cpp!(unsafe [ db as "std::unique_ptr<CStorageView>*"] {
+            (*db)->Flush();
+        });
+    }
+}
