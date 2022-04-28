8a28aaacc75aa519f4ff2d122dbb946c0442b2b9
Ravi Shankar <wafflespeanut@gmail.com>
2022-04-28 08:51:02 +0530

    Get rid of oneof and setup types bridge

--- ain-grpc/src/codegen.rs
+++ ain-grpc/src/codegen.rs
@@ -1,8 +1,4 @@
-use serde::Serialize;
-
-pub mod union {
-    tonic::include_proto!("union");
-}
+// use serde::Serialize;
 
 pub mod types {
     tonic::include_proto!("types");
@@ -12,51 +8,51 @@ pub mod rpc {
     tonic::include_proto!("rpc");
 }
 
-impl Serialize for union::BlockResult {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        match &self.result {
-            Some(union::block_result::Result::HexData(ref data)) => serializer.serialize_str(data),
-            Some(union::block_result::Result::Block(ref block)) => block.serialize(serializer),
-            None => serializer.serialize_str(""),
-        }
-    }
-}
+// impl Serialize for union::BlockResult {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: serde::Serializer,
+//     {
+//         match &self.result {
+//             Some(union::block_result::Result::HexData(ref data)) => serializer.serialize_str(data),
+//             Some(union::block_result::Result::Block(ref block)) => block.serialize(serializer),
+//             None => serializer.serialize_str(""),
+//         }
+//     }
+// }
 
-impl Serialize for union::Transaction {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        match &self.info {
-            Some(union::transaction::Info::Hash(ref data)) => serializer.serialize_str(data),
-            Some(union::transaction::Info::Raw(ref transaction)) => {
-                transaction.serialize(serializer)
-            }
-            None => serializer.serialize_str(""),
-        }
-    }
-}
+// impl Serialize for union::Transaction {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: serde::Serializer,
+//     {
+//         match &self.info {
+//             Some(union::transaction::Info::Hash(ref data)) => serializer.serialize_str(data),
+//             Some(union::transaction::Info::Raw(ref transaction)) => {
+//                 transaction.serialize(serializer)
+//             }
+//             None => serializer.serialize_str(""),
+//         }
+//     }
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::union::{Transaction, transaction, BlockResult, block_result};
+// #[cfg(test)]
+// mod tests {
+//     use super::union::{Transaction, transaction, BlockResult, block_result};
 
-    #[test]
-    fn test_block_result_json() {
-        let foo = BlockResult {
-            result: Some(block_result::Result::HexData("foo".into())),
-        };
-        assert_eq!(serde_json::to_value(&foo).unwrap(), "foo");
-    }
+//     #[test]
+//     fn test_block_result_json() {
+//         let foo = BlockResult {
+//             result: Some(block_result::Result::HexData("foo".into())),
+//         };
+//         assert_eq!(serde_json::to_value(&foo).unwrap(), "foo");
+//     }
 
-    #[test]
-    fn test_transaction() {
-        let foo = Transaction {
-            info: Some(transaction::Info::Hash("foo".into())),
-        };
-        assert_eq!(serde_json::to_value(&foo).unwrap(), "foo");
-    }
-}
+//     #[test]
+//     fn test_transaction() {
+//         let foo = Transaction {
+//             info: Some(transaction::Info::Hash("foo".into())),
+//         };
+//         assert_eq!(serde_json::to_value(&foo).unwrap(), "foo");
+//     }
+// }
