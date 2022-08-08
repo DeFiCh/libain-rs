4b1cdaf1c7ac4e6500e97b2e41a9e6ef75d47c81
Ravi Shankar <wafflespeanut@gmail.com>
2022-08-08 13:27:03 +0530

    Switch to CAmount

---
+++ ain-core/src/coin.rs
@@ -0,0 +1,96 @@
+//! Ported from https://github.com/DeFiCh/ain/blob/ea0c12f843970de53b613cad11a6f2f727a59e89/src/amount.h
+
+use ethnum::U256;
+
+use std::ops::{Add, Sub, Mul, Div};
+
+/// Amount in satoshis (can be negative)
+#[derive(Clone, Copy, PartialEq, Eq, Debug, PartialOrd, Ord)]
+pub struct Amount(pub i64);
+
+impl Amount {
+    pub const COIN: Amount = Amount(100000000);
+
+    pub fn checked_price_multiply(self, price: Self) -> Self {
+        Amount(*U256::new((self.0 as u128 * price.0 as u128) / Self::COIN.0 as u128).low() as i64)
+    }
+
+    pub fn checked_price_divide(self, price: Self) -> Option<Self> {
+        if price.0 == 0 {
+            return None;
+        }
+
+        Some(Amount(*U256::new((self.0 as u128 * Self::COIN.0 as u128) / price.0 as u128).low() as i64))
+    }
+
+    pub fn as_raw(&self) -> f64 {
+        self.0 as f64 / Self::COIN.0 as f64
+    }
+
+    pub fn from_raw(f: f64) -> Self {
+        Amount((f * Self::COIN.0 as f64) as i64)
+    }
+}
+
+// Ops impl
+impl Add for Amount {
+    type Output = Self;
+    fn add(self, other: Self) -> Self::Output {
+        Amount(self.0 + other.0)
+    }
+}
+impl Sub for Amount {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self::Output {
+        Amount(self.0 - other.0)
+    }
+}
+impl Mul for Amount {
+    type Output = Self;
+    fn mul(self, other: Self) -> Self::Output {
+        Amount(self.0 * other.0)
+    }
+}
+impl Div for Amount {
+    type Output = Self;
+    fn div(self, other: Self) -> Self::Output {
+        Amount(self.0 / other.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Amount;
+
+    #[test]
+    fn test_multiply() {
+        let cases = &[
+            (50, 50, 2500),
+        ];
+
+        for &(a, b, r) in cases {
+            let a = Amount(a);
+            let b = Amount(b) * Amount::COIN;
+            assert_eq!(a.checked_price_multiply(b).0, r);
+        }
+    }
+
+    #[test]
+    fn test_divide() {
+        let cases = &[
+            (0, 0, 0),
+            (50, 50, 1),
+        ];
+
+        for &(a, b, r) in cases {
+            let a = Amount(a);
+            let b = Amount(b) * Amount::COIN;
+            if r == 0 {
+                assert!(a.checked_price_divide(b).is_none());
+            } else {
+                let v = a.checked_price_divide(b).unwrap();
+                assert_eq!(v.0, r);
+            }
+        }
+    }
+}
--- ain-core/src/lib.rs
+++ ain-core/src/lib.rs
@@ -1,59 +1,67 @@
+mod coin;
+
+use self::coin::Amount;
+
+use std::error::Error;
 use std::sync::RwLock;
 
 lazy_static::lazy_static! {
-    static ref COEFF_DEX_FEE: RwLock<f64> = RwLock::new(1.8);
-    static ref COEFF_DISCOUNT: RwLock<f64> = RwLock::new(500.0);
-    static ref COEFF_PREMIUM: RwLock<f64> = RwLock::new(3.4);
+    static ref COEFF_DEX_FEE: RwLock<Amount> = RwLock::new(Amount(180000000));
+    static ref COEFF_DISCOUNT: RwLock<Amount> = RwLock::new(Amount(50000000000));
+    static ref COEFF_PREMIUM: RwLock<Amount> = RwLock::new(Amount(340000000));
 }
+
+const DUSD_PRICE_FLOOR: Amount = Amount(99000000);
+const DUSD_PRICE_CEIL: Amount = Amount(105000000);
+const DUSD_PRICE_IDEAL: Amount = Amount(101000000);
+const RATIO_HALF: Amount = Amount(Amount::COIN.0 / 2);
 
 #[cxx::bridge]
 mod calc {
     extern "Rust" {
-        fn set_fee_coefficient(coefficient: f64);
+        fn set_fee_coefficient(coefficient: i64);
 
-        fn set_interest_rate_coefficients(discount: f64, premium: f64);
+        fn set_interest_rate_coefficients(discount: i64, premium: i64);
 
-        fn calc_dex_fee(algo_dusd: f64, dusd_supply: f64) -> f64;
+        fn calc_dex_fee(algo_dusd: i64, dusd_supply: i64) -> Result<i64>;
 
         fn calc_loan_interest_rate(
-            reserve_dfi: f64,
-            reserve_dusd: f64,
-            dfi_oracle_price: f64,
-        ) -> f64;
+            reserve_dfi: i64,
+            reserve_dusd: i64,
+            dfi_oracle_price: i64,
+        ) -> Result<i64>;
     }
 }
 
 /// **DFIP-2206-D**
 ///
 /// Sets the coefficient for calculation of dex stabilization fee. Default is `1.8`
-fn set_fee_coefficient(coefficient: f64) {
-    *COEFF_DEX_FEE.write().unwrap() = coefficient;
+fn set_fee_coefficient(coefficient: i64) {
+    *COEFF_DEX_FEE.write().unwrap() = Amount(coefficient);
 }
 
 /// **DFIP-2206-E**
 ///
 /// Sets the coefficients for discount and premium of DUSD for calculation of interest rates.
 /// Default is `500` for discount and `3.4` for premium.
-fn set_interest_rate_coefficients(discount: f64, premium: f64) {
-    *COEFF_DISCOUNT.write().unwrap() = discount;
-    *COEFF_PREMIUM.write().unwrap() = premium;
+fn set_interest_rate_coefficients(discount: i64, premium: i64) {
+    *COEFF_DISCOUNT.write().unwrap() = Amount(discount);
+    *COEFF_PREMIUM.write().unwrap() = Amount(premium);
 }
 
 /// **DFIP-2206-D**
 ///
 /// Calculates the dynamic dex stabilization fee for DUSD on DUSD-DFI pair. The size of the fee
 /// is determined by the ratio of algorithmic DUSD to the total amount of outstanding DUSD.
-fn calc_dex_fee(algo_dusd: f64, dusd_supply: f64) -> f64 {
-    if dusd_supply <= 0.0 {
-        log::warn!("DUSD supply must be positive. Received {}", dusd_supply);
-        return 0.0;
-    }
-    let ratio = 1.0 - (algo_dusd / dusd_supply);
-    let coeff = *COEFF_DEX_FEE.read().unwrap();
-    if ratio > 0.5 {
-        coeff.powf(ratio - 0.5) - 1.0
+fn calc_dex_fee(algo_dusd: i64, dusd_supply: i64) -> Result<i64, Box<dyn Error>> {
+    let ratio = Amount::COIN - Amount(algo_dusd).checked_price_divide(Amount(dusd_supply))
+        .ok_or_else(||"Cannot divide given DUSD supply")?;
+
+    let coeff = COEFF_DEX_FEE.read().unwrap().as_raw();
+    if ratio > RATIO_HALF {
+        Ok(Amount::from_raw(coeff.powf((ratio - RATIO_HALF).as_raw()) - 1.0).0)
     } else {
-        0.0
+        Ok(0)
     }
 }
 
@@ -61,22 +69,25 @@ fn calc_dex_fee(algo_dusd: f64, dusd_supply: f64) -> f64 {
 ///
 /// Calculates the dynamic interest rates on DUSD loans, based on the current discount/premium
 /// of DUSD evaluated with the DFI price oracle.
-fn calc_loan_interest_rate(reserve_dfi: f64, reserve_dusd: f64, dfi_oracle_price: f64) -> f64 {
-    if reserve_dusd <= 0.0 {
-        log::warn!("Reserve DUSD must be positive. Received {}", reserve_dusd);
-        return 0.0;
+fn calc_loan_interest_rate(reserve_dfi: i64, reserve_dusd: i64, dfi_oracle_price: i64) -> Result<i64, Box<dyn Error>> {
+    if reserve_dfi <= 0 || reserve_dusd <= 0 {
+        return Err("Reserve token amount must be positive".into());
     }
-    let dex_price = (reserve_dfi / reserve_dusd) * dfi_oracle_price;
-    if dex_price < 0.99 {
-        let coeff = *COEFF_DISCOUNT.read().unwrap();
-        coeff.powf(0.99 - dex_price) - 1.0
-    } else if dex_price < 1.01 {
-        0.0
-    } else if dex_price < 1.05 {
-        let coeff = *COEFF_PREMIUM.read().unwrap();
-        1.0 - coeff.powf(dex_price - 1.01)
+
+    let price = Amount(reserve_dfi).checked_price_divide(Amount(reserve_dusd))
+        .map(|a| a.checked_price_multiply(Amount(dfi_oracle_price)))
+        .ok_or_else(|| "Token amount is out of bounds")?;
+
+    if price < DUSD_PRICE_FLOOR {
+        let coeff = COEFF_DISCOUNT.read().unwrap().as_raw();
+        Ok(Amount::from_raw(coeff.powf((DUSD_PRICE_FLOOR - price).as_raw()) - 1.0).0)
+    } else if price < DUSD_PRICE_IDEAL {
+        Ok(0)
+    } else if price < DUSD_PRICE_CEIL {
+        let coeff = COEFF_PREMIUM.read().unwrap().as_raw();
+        Ok(Amount::from_raw(1.0 - coeff.powf((price - DUSD_PRICE_IDEAL).as_raw())).0)
     } else {
-        -0.05
+        Ok(-5000000)
     }
 }
 
@@ -87,47 +98,43 @@ mod tests {
     #[test]
     fn test_default_fees() {
         let cases = &[
-            (510.0, 0.0),
-            (500.0, 0.0),
-            (490.0, 0.005895),
-            (480.0, 0.011825),
-            (450.0, 0.029825),
-            (400.0, 0.06054),
-            (350.0, 0.092172),
-            (250.0, 0.158292),
-            (100.0, 0.265054),
+            (510, 0),
+            (500, 0),
+            (490, 589517),
+            (480, 1182510),
+            (450, 2982546),
+            (400, 6054048),
+            (350, 9217159),
+            (250, 15829218),
+            (100, 26505381),
         ];
 
         for &(algo_dusd, fee) in cases {
-            let f = calc_dex_fee(algo_dusd, 1000.0);
-            if (fee - f).abs() > 0.000001 {
-                panic!("Fee {} is off from expected fee {}", f, fee);
-            }
+            let f = calc_dex_fee(algo_dusd, 1000).unwrap();
+            assert_eq!(f, fee);
         }
     }
 
     #[test]
     fn test_default_rates() {
         let cases = &[
-            (1.05, -0.05),
-            (1.04, -0.037395),
-            (1.03, -0.024777),
-            (1.02, -0.012313),
-            (1.01, 0.0),
-            (0.99, 0.0),
-            (0.98, 0.0641178),
-            (0.97, 0.1323466),
-            (0.95, 0.282209),
-            (0.9, 0.749473),
-            (0.8, 2.2569),
-            (0.7, 5.063192),
+            (105000000, -5000000),
+            (104000000, -3739551),
+            (103000000, -2477749),
+            (102000000, -1231294),
+            (101000000, 0),
+            (99000000, 0),
+            (98000000, 6411778),
+            (97000000, 13234665),
+            (95000000, 28220893),
+            (90000000, 74947322),
+            (80000000, 225689907),
+            (70000000, 506319172),
         ];
 
         for &(price, rate) in cases {
-            let r = calc_loan_interest_rate(1.0, 1.0, price);
-            if (rate - r).abs() > 0.000001 {
-                panic!("Rate {} is off from expected {}", r, rate);
-            }
+            let r = calc_loan_interest_rate(1, 1, price).unwrap();
+            assert_eq!(r, rate);
         }
     }
 }
